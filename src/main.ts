06b3f12483b58eea7654433afcf6442201cd1725
Настя Скрицька <snastia.m@gmail.com>
2024-11-16 12:08:23 +0200

    'vb;vc';b

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// 1
+
+function getPromise (): Promise<(string | number[])> {
+    return new Promise((resolve) => {
+      resolve(['Text', 50]);
+    });
+   }
+
+   getPromise ()
+   .then((data) => {
+    console.log(data);
+   });
